fa9132fd6b93c774f0d8fd72d247daa0b78a134f
barboryna <barbora.grigaliunu@gmail.com>
2015-09-24 12:15:25 +0300

    Prideta funkcija istrizaines skaiciavimui

--- staciakampis.cpp
+++ staciakampis.cpp
@@ -7,13 +7,17 @@ int plotas(int a,int b){
 int perimetras(int a,int b){
     return a*2+b*2;
 }
+double istrizaine(int a,int b){
+    return sqrt((a*a)+(b*b));
+}
 int main()
 {
     int a, b;
     cout << "Iveskite staciakampio ilgi ir ploti:" << endl;
     cin >> a >> b;
     cout << "Plotas: " << plotas(a, b) << endl;
-    cout << "Perimetras: " << perimetras(a, b);
+    cout << "Perimetras: " << perimetras(a, b)<< endl;
+    cout << "Istrizaines ilgis: " << istrizaine(a, b) << endl;
     return 0;
 
 }
