1091b5fa846535a3d639238233ad89bf241ab85c
Crno <info@baltixy.com>
2015-09-24 12:12:21 +0300

    + Perimetras

--- staciakampis.cpp
+++ staciakampis.cpp
@@ -4,12 +4,16 @@ using namespace std;
 int plotas(int a,int b){
     return a*b;
 }
+int perimetras(int a,int b){
+    return a*2+b*2;
+}
 int main()
 {
     int a, b;
     cout << "Iveskite staciakampio ilgi ir ploti:" << endl;
     cin >> a >> b;
-    cout << "Plotas: " << plotas(a, b);
+    cout << "Plotas: " << plotas(a, b) << endl;
+    cout << "Perimetras: " << perimetras(a, b);
     return 0;
 
 }
